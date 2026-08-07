a084154a2cd2d3b443da6fae4178a2cb301d4f44
surya <suryasaibalaji@gmail.com>
2026-08-07 08:39:21 +0530

    Completed Movie CRUD APIs

--- server/controllers/movieControllers.js
+++ server/controllers/movieControllers.js
@@ -1,5 +1,7 @@
 const Movie = require("../models/Movie");
 
+
+// Add Movie
 const addMovie = async (req, res) => {
     try {
         const movie = await Movie.create(req.body);
@@ -9,6 +11,7 @@ const addMovie = async (req, res) => {
             message: "Movie added successfully",
             movie,
         });
+
     } catch (error) {
         res.status(500).json({
             success: false,
@@ -17,6 +20,117 @@ const addMovie = async (req, res) => {
     }
 };
 
+
+// Get All Movies
+const getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find();
+
+        res.status(200).json({
+            success: true,
+            count: movies.length,
+            movies,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+// Get One Movie
+const getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+        if (!movie) {
+            return res.status(404).json({
+                success: false,
+                message: "Movie not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            movie,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+// Update Movie
+const updateMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!movie) {
+            return res.status(404).json({
+                success: false,
+                message: "Movie not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Movie updated successfully",
+            movie,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+// Delete Movie
+const deleteMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+
+        if (!movie) {
+            return res.status(404).json({
+                success: false,
+                message: "Movie not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Movie deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
 module.exports = {
     addMovie,
+    getMovies,
+    getMovieById,
+    updateMovie,
+    deleteMovie,
 };
--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -1,8 +1,34 @@
 const express = require("express");
+
 const router = express.Router();
 
-const { addMovie } = require("../controllers/movieControllers");
+const {
+    addMovie,
+    getMovies,
+    getMovieById,
+    updateMovie,
+    deleteMovie
+} = require("../controllers/movieControllers");
 
+
+// POST - Add Movie
 router.post("/", addMovie);
+
+
+// GET - All Movies
+router.get("/", getMovies);
+
+
+// GET - One Movie
+router.get("/:id", getMovieById);
+
+
+// PUT - Update Movie
+router.put("/:id", updateMovie);
+
+
+// DELETE - Delete Movie
+router.delete("/:id", deleteMovie);
+
 
 module.exports = router;
